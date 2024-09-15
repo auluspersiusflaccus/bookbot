43900a83d17b1f0df70a5abaa9bbec4924cc06be
Auluspersiusflaccus <felix.charron-ducharme.1@ulaval.ca>
2024-09-15 18:35:08 -0400

    started main function

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    # Your main program logic goes here
+
+    with open(books/frankenstein.txt) as f:
+        file_contents = f.read()
+    pass
+
+    
+if __name__ == "__main__":
+    main()
